174dc649b05708fac5ee7550203b1f4c77d9e6a1
Lil1ian <Lil1ian@users.noreply.github.com>
2017-04-28 20:46:13 +0800

    Update Maximum Depth of Binary Tree.java

--- Binary Search/Maximum Depth of Binary Tree.java
+++ Binary Search/Maximum Depth of Binary Tree.java
@@ -20,3 +20,35 @@ public class Solution {
         return Math.max(rightM, leftM)+1;
     }
 }
+
+/**
+ *iterative
+ */
+
+public class Solution {
+    public int maxDepth(TreeNode root) {
+        if(root==null) return 0;
+        int level = 0;
+        LinkedList queue = new LinkedList();
+        int cur=1, next=0;
+        queue.offer(root);
+        while(!queue.isEmpty()) {
+            TreeNode n = (TreeNode)queue.poll();
+            cur--;
+            if(n.left!=null) {
+                queue.add(n.left);
+                next++;
+            }
+            if(n.right!=null) {
+                queue.add(n.right);
+                next++;
+            }
+            if(cur==0) {
+                cur = next;
+                next = 0;
+                level++;
+            }
+        }
+        return level;
+    }
+}
