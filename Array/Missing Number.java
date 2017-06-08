3bc6a6e4e4129c75778a84e58b09542122431b48
Lil1ian <Lil1ian@users.noreply.github.com>
2017-06-08 17:17:03 +0800

    Create Missing Number.java

--- Array/Missing Number.java
+++ Array/Missing Number.java
@@ -1,3 +1,7 @@
+/**
+ *Math
+ */
+
 public class Solution {
     public int missingNumber(int[] nums) {
         if(nums==null || nums.length==0) return 0;
@@ -7,5 +11,21 @@ public class Solution {
             sum += nums[i];
         }
         return N*(N+1)/2-sum;
+    }
+}
+
+/**
+ *Bit Manipulation
+ */
+
+public class Solution {
+    public int missingNumber(int[] nums) {
+        if(nums==null || nums.length==0) return 0;
+        int N = nums.length;
+        int res = nums.length;
+        for(int i=0; i<N; i++) {
+            res ^= i ^ nums[i];
+        }
+        return res;
     }
 }
