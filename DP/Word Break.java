0516601cfc511474735fc225460db894d88973ae
Lil1ian <Lil1ian@users.noreply.github.com>
2017-08-19 20:19:07 +0800

    Create Word Break.java

---
+++ DP/Word Break.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        boolean[] dp = new boolean[s.length()+1];
+        Arrays.fill(dp,false);
+        dp[s.length()]=true;
+        for(int i = s.length()-1; i >=0 ; i--){
+            for(int j = i; j < s.length(); j++){
+                String sub = s.substring(i,j+1);
+                if(wordDict.contains(sub) && dp[j+1]){
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+        return dp[0];
+    }
+}
