0062be38d8098e945b67567e8ff57f8d9df38890
Lil1ian <Lil1ian@users.noreply.github.com>
2017-08-21 16:37:20 +0800

    Update Valid Palindrome.java

--- String/Valid Palindrome.java
+++ String/Valid Palindrome.java
@@ -10,3 +10,38 @@ public class Solution {
         return true;
     }
 }
+
+/* another solution*/
+
+
+class Solution {
+    
+    public boolean isPalindrome(String s) {
+        // Start typing your Java solution below
+        // DO NOT write main() function
+        
+        if(s.equals("")) return true;
+        s = s.toLowerCase();
+        int len = s.length();
+        int left = 0, right = len - 1;
+        while(left < right){
+            while(!isValid(s.charAt(left))){
+                left++;
+                if(left >= right) return true;
+            } 
+            while(!isValid(s.charAt(right))){
+                right--;
+                if(left >= right) return true;
+            }
+            if(s.charAt(left) != s.charAt(right)) return false;
+            left++;
+            right--;
+        }
+        return true;
+    }
+    public boolean isValid(char c) {
+            if(c >= 'a' && c <= 'z') return true;
+            if(c >= '0' && c <= '9') return true;
+            return false;
+    }
+}
