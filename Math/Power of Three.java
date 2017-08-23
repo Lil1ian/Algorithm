e6a4a921aab37d0da20c4f8c9490b262833589ce
Lil1ian <Lil1ian@users.noreply.github.com>
2017-08-23 20:44:12 +0800

    Create Power of Three.java

---
+++ Math/Power of Three.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isPowerOfThree(int n) {
+        if (n <= 0) {
+            return false;
+        }
+        while (n != 0) {
+            if (n == 1) {
+                return true; 
+            } 
+            else if (n % 3 != 0) {
+                return false;
+            } 
+            else {
+                n /= 3;
+            }
+        }
+        return true;
+        
+    }
+}
