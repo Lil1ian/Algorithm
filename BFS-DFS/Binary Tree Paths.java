1975607533353830d120b72c591a73bcfb415451
Lil1ian <Lil1ian@users.noreply.github.com>
2017-11-29 23:29:23 -0800

    Update Binary Tree Paths.java

--- BFS-DFS/Binary Tree Paths.java
+++ BFS-DFS/Binary Tree Paths.java
@@ -7,17 +7,31 @@
  *     TreeNode(int x) { val = x; }
  * }
  */
+
 class Solution {
+    
     List<String> res = new ArrayList<String>();
     public List<String> binaryTreePaths(TreeNode root) {
         
-        if(root==null) return res; 
-        FindPaths(root, root.val + "");
+        if(root == null) {
+            return res;
+        }
+        dfs(root, root.val + "");
         return res;
     }
-    public void FindPaths(TreeNode node, String path) {
-        if(node.left==null && node.right==null) res.add(path);
-        if(node.left!=null) FindPaths(node.left, path + "->" + node.left.val);
-        if(node.right!=null) FindPaths(node.right, path + "->" + node.right.val);
+    
+    public List<String> dfs(TreeNode node, String path) {
+        if(node.left==null && node.right==null) {
+            res.add(path);
+        }
+        if(node.left != null) {
+            dfs(node.left, path + "->" + node.left.val);
+        }
+        if(node.right != null) {
+            dfs(node.right, path + "->" + node.right.val);
+        } 
+        return res;
     }
+       
 }
+
