dda7fb6c661ea2d7bc93ed8829daf9f1cdbaded3
Lil1ian <racheliu63@outlook.com>
2018-07-01 21:43:02 -0700

    Update Valid Parentheses.java

--- Stack/Valid Parentheses.java
+++ Stack/Valid Parentheses.java
@@ -1,21 +1,31 @@
 public class Solution {
     public boolean isValid(String s) {
-        if(s.length()==1 || s==null) return false;
+        if(s.length()==1 || s==null) {
+            return false;
+        }
         int n = s.length();
         Stack<Character> t = new Stack<Character>();
         for(int i=0; i<n; i++) {
             if(s.charAt(i)=='(' || s.charAt(i)=='[' || s.charAt(i)=='{') t.push(s.charAt(i));
             else {
-                if(t.size()==0) return false;
+                if(t.size()==0) {
+                    return false;
+                }
                 char top = t.pop();
                 if(s.charAt(i)==')') {
-                    if(top!='(') return false;
+                    if(top!='(') {
+                        return false;
+                    }
                 }
                 if(s.charAt(i)==']') {
-                    if(top!='[') return false;
+                    if(top!='[') {
+                        return false;
+                    }
                 }
                 if(s.charAt(i)=='}') {
-                    if(top!='{') return false;
+                    if(top!='{') {
+                        return false;
+                    }
                 }
             }
         }
