fc18de884c8f6ad3fc5a5ae4f3a09e8871f74f4f
Lil1ian <racheliu63@outlook.com>
2018-07-01 21:31:33 -0700

    Update Word Break.java

--- DP/Word Break.java
+++ DP/Word Break.java
@@ -1,6 +1,7 @@
 class Solution {
     public boolean wordBreak(String s, List<String> wordDict) {
         boolean[] dp = new boolean[s.length()+1];
+        
         Arrays.fill(dp,false);
         dp[s.length()]=true;
         for(int i = s.length()-1; i >=0 ; i--){
